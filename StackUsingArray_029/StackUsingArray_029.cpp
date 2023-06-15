9b9601d24d4276464df992a033b80b215b810924
RamaSandhy <rshandy07@gmail.com>
2023-06-15 10:38:17 +0700

    Menambahkan metode 'push' ke class Program, yang memungkinkan mendorong element ke tumpukan. Juga termasuk penanganan kesalahan untuk tumpukan penuh.

--- StackUsingArray_029/StackUsingArray_029.cpp
+++ StackUsingArray_029/StackUsingArray_029.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+class Program {
+private:
+	string stack_array[5];
+	int top;
+
+public:
+	Program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "Stacl Full" << endl;
+			return"";
+		}
+
+		top++;
+		stack_array[top] = element;
+
+		cout << endl;
+		cout << endl;
+
+		return element;
+	}
+};
